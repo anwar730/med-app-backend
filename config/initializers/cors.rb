a1dc361bbfa1b34c2b56365c78f0011b9ca42a4f
Anwar <anwaarismail559@gmail.com>
2026-07-16 17:44:15 +0300

    update cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,26 +1,37 @@
-# Be sure to restart your server when you modify this file.
+# # Be sure to restart your server when you modify this file.
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+# # Avoid CORS issues when API is called from the frontend app.
+# # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
 
-# Read more: https://github.com/cyu/rack-cors
+# # Read more: https://github.com/cyu/rack-cors
 
+# # Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# #   allow do
+# #     origins "example.com"
+# #
+# #     resource "*",
+# #       headers: :any,
+# #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+# #   end
+# # end
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #   allow do
-#     origins "example.com"
-#
+#     origins "http://localhost:5173"  # your frontend
 #     resource "*",
 #       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       expose: ["Authorization"]
 #   end
 # end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"  # your frontend
+    origins "http://localhost:5173",
+            "https://med-app-frontend-chi.vercel.app"
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ["Authorization"]
   end
 end
-
